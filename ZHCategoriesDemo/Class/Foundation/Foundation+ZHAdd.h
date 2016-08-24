108e993f4e60f6e3f8da9296c0ac54868caad076
Walen Wu <walen.wu@hirede.com>
2016-08-24 11:03:27 +0800

    增加一个对外头文件

---
+++ ZHCategoriesDemo/Class/Foundation/Foundation+ZHAdd.h
@@ -0,0 +1,19 @@
+//
+//  Foundation+ZHAdd.h
+//  RCBox
+//
+//  Created by walen on 16/8/24.
+//  Copyright © 2016年 walen. All rights reserved.
+//
+
+#ifndef Foundation_ZHAdd_h
+#define Foundation_ZHAdd_h
+
+#import "NSString+ZHAdd.h"
+#import "NSBundle+ZHAdd.h"
+#import "NSObject+ZHAddForKVO.h"
+#import "NSObject+ZHAddForMethodSwizzing.h"
+#import "NSThread+ZHAddForRunloop.h"
+#import "NSNotificationCenter+AutoRemove.h"
+
+#endif /* Foundation_ZHAdd_h */
---
+++ ZHCategoriesDemo/Class/UIKit/UIKit+ZHAdd.h
@@ -0,0 +1,22 @@
+//
+//  UIKit+ZHAdd.h
+//  RCBox
+//
+//  Created by walen on 16/8/24.
+//  Copyright © 2016年 walen. All rights reserved.
+//
+
+#ifndef UIKit_ZHAdd_h
+#define UIKit_ZHAdd_h
+
+#import "CALayer+ZHAdd.h"
+#import "UIApplication+ZHAdd"
+#import "UIColor+ZHAdd.h"
+#import "UIControl+ZHAdd.h"
+#import "UIButton+ZHAdd"
+#import "UIImage+ZHAdd.h"
+#import "UIView+ZHAdd.h"
+#import "UIKit+Property.h"
+#import "UIViewController+ZHAdd.h"
+
+#endif /* UIKit_ZHAdd_h */
