578be7eebc22db8aa3bbba01328e71ec9a080fdd
Walen Wu <walen.wu@hirede.com>
2016-08-24 11:05:59 +0800

    勘误

--- ZHCategoriesDemo/Class/UIKit/UIKit+ZHAdd.h
+++ ZHCategoriesDemo/Class/UIKit/UIKit+ZHAdd.h
@@ -10,10 +10,10 @@
 #define UIKit_ZHAdd_h
 
 #import "CALayer+ZHAdd.h"
-#import "UIApplication+ZHAdd"
+#import "UIApplication+ZHAdd.h"
 #import "UIColor+ZHAdd.h"
 #import "UIControl+ZHAdd.h"
-#import "UIButton+ZHAdd"
+#import "UIButton+ZHAdd.h"
 #import "UIImage+ZHAdd.h"
 #import "UIView+ZHAdd.h"
 #import "UIKit+Property.h"
