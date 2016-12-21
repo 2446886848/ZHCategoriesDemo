639a11ef32d0df919f1fa1e2aa16d11141fd70ff
Walen Wu <walen.wu@hirede.com>
2016-12-21 11:54:47 +0800

    加入统一的头文件

---
+++ ZHCategoriesDemo/Class/ZHCategory.h
@@ -0,0 +1,15 @@
+//
+//  ZHCategory.h
+//  ZHCategoriesDemo
+//
+//  Created by walen on 16/12/21.
+//  Copyright © 2016年 wuzhihe. All rights reserved.
+//
+
+#ifndef ZHCategory_h
+#define ZHCategory_h
+
+#import "Foundation+ZHAdd.h"
+#import "UIKit+ZHAdd.h"
+
+#endif /* ZHCategory_h */
